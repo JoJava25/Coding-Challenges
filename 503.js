114673a4d3f6ceee0d6a5ff3c86b7ac8fac696e6
JoJava25 <trogdor2k5@hotmail.com>
2022-05-03 12:16:56 -0400

    New code

---
+++ 503.js
@@ -0,0 +1,9 @@
+function longest(words) {
+    let longest = 0
+    for(let i = 0; i < words.length; i++){
+      if(words[i].length > longest){
+        longest = words[i].length
+      }
+    }
+    return longest
+  }
