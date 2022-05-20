8c025d7938b9fd5f6432d34dcd793fe53395db51
JoJava25 <trogdor2k5@hotmail.com>
2022-05-20 12:39:04 -0400

    Neew code

---
+++ ConsecutiveItems.js
@@ -0,0 +1,9 @@
+function consecutive(arr, a, b) {
+    const arrA = arr.indexOf(a)
+    const arrB = arr.indexOf(b)
+    if(arrA + 1 == arrB || arrB + 1 == arrA){
+      return true
+    }else{
+      return false
+    }
+  }
