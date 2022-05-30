407423d5c7ad72568916cd7144e62c43e6fd42a2
JoJava25 <trogdor2k5@hotmail.com>
2022-05-30 14:56:13 -0400

    New code

---
+++ Piper.js
@@ -0,0 +1,14 @@
+var countDeafRats = function(town) {
+    // Your code here
+    if(town != null){
+      [left,right]=town.split('P');
+          var a=left+right.split('').reverse().join('');
+          var b=(a.match(/O~|~O/gi)||[]).filter(v=>v=='O~').length;
+           return b;
+    }
+    return 0;
+    
+          
+          
+         
+  }
