e9df2f3336c4cb2789e2fb7c8e57819954d3dae2
JoJava25 <trogdor2k5@hotmail.com>
2022-06-06 12:59:10 -0400

    New solution

---
+++ SimpleFun396.js
@@ -0,0 +1,6 @@
+const longestSubstringOf = ([...s], visited = new Map(), start = 0) => s.reduce((res, curr, end) => {
+    if (visited[curr] != undefined) start = Math.max(visited[curr] + 1, start);   
+    if (res.length < end - start + 1) res = s.slice(start, end + 1); 
+    visited[curr] = end;
+    return res;
+  }, '').length;
