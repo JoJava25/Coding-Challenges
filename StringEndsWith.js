2060ff91d28f30d7555f73199fca9c18339be31c
JoJava25 <trogdor2k5@hotmail.com>
2022-06-11 23:50:24 -0400

    New solution

---
+++ StringEndsWith.js
@@ -0,0 +1,9 @@
+function solution(str, ending){
+    let size = ending.length
+    const test = str.split('').splice(str.length - size, size).join('')
+    if(test === ending){
+      return true
+    }else{
+      return false
+    }
+  }
