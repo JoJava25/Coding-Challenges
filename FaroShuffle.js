a3709451703bae8e487c7aec5c9e0be7f964927e
JoJava25 <trogdor2k5@hotmail.com>
2022-07-14 13:50:40 -0400

    New solution

---
+++ FaroShuffle.js
@@ -0,0 +1,22 @@
+function faroCount(deckSize) {
+    let cards = Array(deckSize).fill().map((e, i) => i);
+    let halfLength = cards.length / 2;
+    let cardsMerged = Object.assign(cards);
+    let count = 0;
+
+    do {
+        let firstHalf = cardsMerged.slice(0, halfLength);
+        let secondHalf = cardsMerged.slice(halfLength);
+
+        cardsMerged = [];
+        firstHalf.forEach((card, i) => {
+            cardsMerged.push(card, secondHalf[i]);
+        })
+
+        
+        count++;
+
+    } while ( !cardsMerged.every( (e, i) => e == cards[i]) )
+        
+    return count;
+}
