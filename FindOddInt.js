77587db68e9053f84f3d8db2921565a546ae63be
JoJava25 <trogdor2k5@hotmail.com>
2022-08-09 12:38:30 -0400

    New solution

---
+++ FindOddInt.js
@@ -0,0 +1,16 @@
+function findOdd(A) {
+    let freq = {}
+      for(let i = 0; i < A.length; i++){
+        let num = A[i]
+        if(freq[num]){
+          freq[num]++
+          }else if(! freq[num]){
+            freq[num] = 1
+          }
+      }
+      for (const property in freq){
+        if(freq[property] % 2 !== 0){
+          return Number(property)
+        }
+      }
+    }
