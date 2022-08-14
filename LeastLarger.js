f6ea6894e95d9d615c2994469bbd76de912ca582
JoJava25 <trogdor2k5@hotmail.com>
2022-08-14 12:47:09 -0400

    New solution

---
+++ LeastLarger.js
@@ -0,0 +1,7 @@
+function leastLarger(a, i) {
+    const targetVal = a[i]
+    const largerVals = a.filter(num => num > targetVal)
+    const leastLargeVal = Math.min(...largerVals)
+    
+    return a.findIndex(num => num === leastLargeVal)
+}
