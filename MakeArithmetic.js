9f0131bcdba264c61172148dc138dbfcddd18259
JoJava25 <trogdor2k5@hotmail.com>
2022-09-29 23:25:41 -0400

    New solution

---
+++ MakeArithmetic.js
@@ -0,0 +1,18 @@
+function arithmetic(a, b, operator){
+    var mathFun = {
+      'add': function (a, b) {
+          return a+b;
+      },
+      'subtract': function (a, b) {
+          return a-b;
+      },
+      'multiply': function (a, b) {
+          return a*b;
+      }, 
+      'divide': function (a, b) {
+          return a/b;
+      }
+    }
+  
+    return mathFun[operator](a,b);
+  }
