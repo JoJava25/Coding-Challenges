a99ba307e3fd73de47052ff35e4e57a1d36a73df
JoJava25 <trogdor2k5@hotmail.com>
2022-10-01 23:46:54 -0400

    New solution

---
+++ Reverse Words2.js
@@ -0,0 +1,13 @@
+function reverseWords(str) {
+    let reversedWord = '';
+    let reversedStr = '';
+    for (let i = 0; i < str.length; i++) {
+      if (str[i] !== ' ') {
+        reversedWord = str[i] + reversedWord;
+      } else {
+        reversedStr += reversedWord + ' ';
+        reversedWord = '';
+      }
+    }
+    return reversedStr + reversedWord;
+  }
