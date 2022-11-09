b5b05ee1f16ceb13abcc7461a93584271e9f644e
JoJava25 <trogdor2k5@hotmail.com>
2022-11-09 01:06:27 -0500

    New solution

---
+++ MakeEveryoneHappy.js
@@ -0,0 +1,19 @@
+function smile(text) {
+    let arr = text.split('')
+    for(let i = 0; i < arr.length; i++){
+      if(arr[i] === ":" || arr[i] === ";" || arr[i] === "="){
+        if(arr[i+1] === "-" || arr[i+1] === "~"){
+          if(arr[i+2] === "(" ){
+            arr[i+2] = ")"
+          }else if(arr[i+2] === "[" ){
+            arr[i+2] = "]"
+          }
+        }else if(arr[i+1] === "(" ){
+          arr[i+1] = ")"
+        }else if(arr[i+1] === "[" ){
+          arr[i+1] = "]"
+        }
+      }
+    }
+    return arr.join('')
+  }
