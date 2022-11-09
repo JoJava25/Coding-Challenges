0db7df77261d2a65982c795052c5cd6a0a2bc288
JoJava25 <trogdor2k5@hotmail.com>
2022-11-09 01:07:17 -0500

    Refactor

--- MakeEveryoneHappy.js
+++ MakeEveryoneHappy.js
@@ -1,5 +1,6 @@
 function smile(text) {
     let arr = text.split('')
+    
     for(let i = 0; i < arr.length; i++){
       if(arr[i] === ":" || arr[i] === ";" || arr[i] === "="){
         if(arr[i+1] === "-" || arr[i+1] === "~"){
