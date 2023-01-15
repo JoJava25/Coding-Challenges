7c5facf70c5ef4009b8107f41592fb5af344690c
JoJava25 <trogdor2k5@hotmail.com>
2023-01-14 23:12:12 -0500

    Refactor

--- PeopleOnTheBus.js
+++ PeopleOnTheBus.js
@@ -1,8 +1,7 @@
 let number = function(busStops){
     let count = 0
     for(let i = 0; i < busStops.length; i++){
-        count += busStops[i][0]
-        count -= busStops[i][1]
+        count += busStops[i][0] - busStops[i][1]
       if(count < 0)
         count = 0
     }
