ad1d9900acb4e4b6334f4ac18174b8e0986d5f59
JoJava25 <trogdor2k5@hotmail.com>
2023-01-14 23:09:19 -0500

    New solution

---
+++ PeopleOnTheBus.js
@@ -0,0 +1,10 @@
+let number = function(busStops){
+    let count = 0
+    for(let i = 0; i < busStops.length; i++){
+        count += busStops[i][0]
+        count -= busStops[i][1]
+      if(count < 0)
+        count = 0
+    }
+      return count
+  }
