7aec2c428c34880024a0810cce185895a9b56ec2
JoJava25 <trogdor2k5@hotmail.com>
2023-02-12 23:33:54 -0500

    New solution

---
+++ IntegerDepth.js
@@ -0,0 +1,15 @@
+const computeDepth = (n) => {
+    let digits = new Set()
+    let depth = 1
+    while (digits.size < 10) {
+      let num = depth * n
+      
+      while (num > 0) {
+        digits.add(num % 10)
+        num = Math.floor(num / 10)
+      }
+      
+      depth++
+    }
+    return depth - 1
+  }
