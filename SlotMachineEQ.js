9bbcebe20a0d17649d77711564e71d1bfb13f1b0
JoJava25 <trogdor2k5@hotmail.com>
2023-02-17 23:44:10 -0500

    New solution

---
+++ SlotMachineEQ.js
@@ -0,0 +1,10 @@
+function slot(s){
+    let c = s.match(/([?!])\1*/g).map(e=>e.length).sort((a,b)=>b-a);
+    switch(c.length) {
+      case 1: return 1000;
+      case 2: return c[0]===4?800:500;
+      case 3: return c[0]===3?300:200;
+      case 4: return 100;
+      case 5: return 0;
+    }
+  }
