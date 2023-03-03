881155987fbe46176e4ce50b0fdb293205d1822f
JoJava25 <trogdor2k5@hotmail.com>
2023-03-03 03:12:53 -0500

    New solution

---
+++ YoungestDetective.js
@@ -0,0 +1,13 @@
+function missingWord(nums, str) {
+    let letters =  str.split(' ').join('').toLowerCase().split('')
+    nums = nums.sort((a, b) => a - b)
+    let result = []
+    for(let i = 0; i <= nums.length - 1; i++){
+      if(nums[i] <= letters.length - 1){
+        result.push(letters[nums[i]])
+      }else{
+        return "No mission today"
+      }
+    }
+    return result.join('')
+  }
