868c7b83e5d59d4bf747a40383b597f16cbca507
JoJava25 <trogdor2k5@hotmail.com>
2023-03-10 23:00:21 -0500

    Update

--- KeypadHorror.js
+++ KeypadHorror.js
@@ -1,14 +1,14 @@
 function computerToPhone(numbers){
-    var ans='';
+    var ans=''
     for (var i=0; i<numbers.length; ++i)
     {
-      if (numbers[i]=='1') ans+='7';
-      else if (numbers[i]=='2') ans+='8';
-      else if (numbers[i]=='3') ans+='9';
-      else if (numbers[i]=='7') ans+='1';
-      else if (numbers[i]=='8') ans+='2';
-      else if (numbers[i]=='9') ans+='3';
-      else ans+=numbers[i];
+      if (numbers[i]=='1') ans+='7'
+      else if (numbers[i]=='2') ans+='8'
+      else if (numbers[i]=='3') ans+='9'
+      else if (numbers[i]=='7') ans+='1'
+      else if (numbers[i]=='8') ans+='2'
+      else if (numbers[i]=='9') ans+='3'
+      else ans+=numbers[i]
     }
-    return ans;
+    return ans
   }
