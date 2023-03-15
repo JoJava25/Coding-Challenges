25ea667389576a7c338b3c3b8ed2265bfeacea90
JoJava25 <trogdor2k5@hotmail.com>
2023-03-14 23:40:42 -0400

    Update

--- MystSingularityNums.js
+++ MystSingularityNums.js
@@ -1,3 +1,3 @@
 function realNumbers(m, n=BigInt(m)) {
-    return Number(n - n/2n - n/3n - n/5n + n/6n + n/10n + n/15n - n/30n);
+    return Number(n - n/2n - n/3n - n/5n + n/6n + n/10n + n/15n - n/30n)
   }
