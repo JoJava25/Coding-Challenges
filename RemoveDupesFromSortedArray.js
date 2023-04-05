9e794179e610af7441fb2fb2a0af7dd5dfb6f4e4
JoJava25 <trogdor2k5@hotmail.com>
2023-04-05 14:50:53 -0400

    Refactor

--- RemoveDupesFromSortedArray.js
+++ RemoveDupesFromSortedArray.js
@@ -3,17 +3,18 @@ let removeDuplicates = function(nums){
 
     for(let i = 0; i <= nums.length - 1; i++){
         let current = nums[i]
-
-        if(freq[current] && current != "_"){
-            nums.splice(i, 1)
-            nums.push("_")
-            i--
-        }else if(! freq[current] && current != "_"){
-            freq[current] = 1
-            freq.count++
+        if(current != "_"){
+          if(freq[current]){
+              nums.splice(i, 1)
+              nums.push("_")
+              i--
+          }else if(! freq[current]){
+              freq[current] = 1
+              freq.count++
+          }
         }
     }
-    return [freq.count, nums]
+    return freq.count
 }
 
 
