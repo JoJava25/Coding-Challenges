743b034860483fd6233031bb2f2581e653e683b8
JoJava25 <trogdor2k5@hotmail.com>
2023-04-08 23:15:46 -0400

    New solution

--- ReverseOrRotate.js
+++ ReverseOrRotate.js
@@ -3,7 +3,7 @@ function revrot(str, sz) {
       
     const sumCubes = chunk => chunk.split('').reduce((sum, digit) => sum += digit**3, 0);
     const reverse = chunk => chunk.split('').reverse().join('');
-    const rotate = chunk => chunk.slice(1) + chunk.slice(0, 1)
+    const rotate = chunk => chunk.slice(1) + chunk.slice(0, 1);
     
     const chunks = [];
     
