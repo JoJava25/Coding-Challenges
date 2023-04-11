18f69d3b38d38ed4909a9ba904b11fc54c73ff15
JoJava25 <trogdor2k5@hotmail.com>
2023-04-10 23:24:40 -0400

    New Solution

---
+++ LongestCommonPrefix.js
@@ -0,0 +1,21 @@
+var longestCommonPrefix = function(arr) {
+    let i = 1;
+    let prf = arr[0];
+    while(i < arr.length){
+        if(!arr[i].startsWith(prf)){
+            prf = prf.slice(0, -1)
+        }else{
+            i++
+        }
+    }
+    return prf
+
+
+};
+
+console.log(longestCommonPrefix(["flower", "flow", "flight"]))
+console.log(longestCommonPrefix(["pretend", "preset", "predict"]))
+console.log(longestCommonPrefix(["dog", "cat", "hippo"]))
+console.log(longestCommonPrefix([""]))
+console.log(longestCommonPrefix(["a"]))
+console.log(longestCommonPrefix([","]))
