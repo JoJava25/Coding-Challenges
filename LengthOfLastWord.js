dc6c68952e734ea4b182120b07c769cef30df713
JoJava25 <trogdor2k5@hotmail.com>
2023-04-13 00:30:42 -0400

    Refactor

--- LengthOfLastWord.js
+++ LengthOfLastWord.js
@@ -1,15 +1,15 @@
-let LengthOfLastWord = function(s){
+let lengthOfLastWord = function(s){
     let str = s.trim()
-    console.log(str)
-    for(let i = str.length - 1; i <= 0; i--){
-        console.log(str[i])
+
+    for(let i = str.length - 1; i >= 0; i--){
         if(str[i] === " "){
-            console.log(str.slice(i))
-            return str.slice(i + 1)
+            return str.slice(i + 1).length
         }
     }
+    return str.length
 }
 
-console.log(LengthOfLastWord("Hello World"))
-console.log(LengthOfLastWord("From another planet"))
-console.log(LengthOfLastWord(" what in gods name is   this "))
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord("From another planet"))
+console.log(lengthOfLastWord(" what in gods name is   this "))
+console.log(lengthOfLastWord("a"))
