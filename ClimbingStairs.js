5ef16bcfbecb5cbbc895b5e2c799cee7bda47684
JoJava25 <trogdor2k5@hotmail.com>
2023-05-01 23:32:26 -0400

    New solution

---
+++ ClimbingStairs.js
@@ -0,0 +1,12 @@
+let climbStairs = function(n){
+    let arr = [1,2];
+    for(let i =2; i <= n; i++){
+        arr[i] = arr[i - 1] + arr[i - 2]
+    }
+    return arr[n-1]
+}
+
+
+console.log(climbStairs(2), "(2)")
+console.log(climbStairs(3), "(3)")
+console.log(climbStairs(4), "(5)")
