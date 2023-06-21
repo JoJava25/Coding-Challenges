563cc5d99646fea0235b380cbfb97334ac98e5b0
JoJava25 <trogdor2k5@hotmail.com>
2023-06-21 00:13:24 -0400

    New solution

--- Counter.js
+++ Counter.js
@@ -1,5 +1,11 @@
 let createCounter = function(n) {
     return function() {
-        
+       return n++
     }
 }
+
+const counter = createCounter(10)
+
+console.log(counter()) // 10
+console.log(counter()) // 11
+console.log(counter()) // 12
