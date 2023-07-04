84abb2e7005f40063c2439549f4e014b027db6f7
JoJava25 <trogdor2k5@hotmail.com>
2023-07-03 23:12:43 -0400

    New solution

---
+++ ChunkArray.js
@@ -0,0 +1,15 @@
+let chunk = function(arr, size){
+    let result = []
+    let chunk = []
+    for(let i = 0; i < arr.length; i++){
+        chunk.push(arr[i])
+        if(chunk.length = size){
+            result.push(chunk)
+            chunk = []
+        }
+    }
+    if(chunk.length){
+        result.push(chunk)
+    }
+    return result
+}
