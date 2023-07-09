78ad89ca65d216d1649a8fe6fff9bc6c05b04b66
JoJava25 <trogdor2k5@hotmail.com>
2023-07-08 23:46:23 -0400

    Refactor

--- FilterElementsFromArray.js
+++ FilterElementsFromArray.js
@@ -1,9 +1,9 @@
 let filter = function(arr, fn){
-    let ans = []
+    let ans = [];
     for (var i = 0; i < arr.length; i++) {
       if (fn(arr[i], i)) {
-        ans.push(arr[i])
+        ans.push(arr[i]);
       }
     }
-    return ans
+    return ans;
 }
