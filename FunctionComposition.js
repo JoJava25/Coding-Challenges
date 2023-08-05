572693b424065ac0b9b6d26adec04cc2f35b9339
JoJava25 <trogdor2k5@hotmail.com>
2023-08-04 23:58:39 -0400

    Refactor

--- FunctionComposition.js
+++ FunctionComposition.js
@@ -2,7 +2,6 @@ let compose = function(functions) {
     if (functions.length === 0) {
         return function(x) { return x}
     }
-
     return function(x){
         let result = x
         for(let i = functions.length - 1; i>= 0; i--){
