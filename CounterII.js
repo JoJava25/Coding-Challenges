dc3e11ff109e99992ba0331e6f5335b47bf5e4ea
JoJava25 <trogdor2k5@hotmail.com>
2023-09-11 00:15:28 -0400

    Refactor

--- CounterII.js
+++ CounterII.js
@@ -1,15 +1,25 @@
-var createCounter = function(init) {
-    let obj = {
-        increment = function(){
-            return init++
-        }
-
-        decrement = function(){
-            return init--
-        }
-
-        reset = function(){
-            return init
-        }
+class Counter {
+    constructor(init){
+        this.init = init
+        this.presentCount = init
     }
-};
+
+    increment(){
+        this.presentCount += 1
+        return this.presentCount
+    }
+
+    decrement(){
+        this.presentCount -= 1
+        return this.presentCount
+    }
+
+    reset(){
+        this.presentCount = this.init
+        return this.presentCount
+    }
+}
+
+var createCounter = function(init){
+    return new Counter(init)
+}
