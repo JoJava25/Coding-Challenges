6acd64a14f027914bc3a9c02230b803c337b9112
JoJava25 <trogdor2k5@hotmail.com>
2023-09-18 23:35:04 -0400

    Refactor

--- ChunkArray.js
+++ ChunkArray.js
@@ -1,7 +1,6 @@
 let chunk = function(arr, size){
     let result = []
     let chunk = []
-    
     for(let i = 0; i < arr.length; i++){
         chunk.push(arr[i])
         if(chunk.length == size){
