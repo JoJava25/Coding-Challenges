6f5a12a35b61ce809b3073f9fbe23dabade4a4b7
JoJava25 <trogdor2k5@hotmail.com>
2023-11-08 16:24:42 -0500

    Refactor

--- DiceRotation.js
+++ DiceRotation.js
@@ -1,3 +1,18 @@
 function rotations(dieArray){
-    
+    function rotations(dieArray){
+        let possibleSolutions = [];
+        let tempSum;
+        
+        for(let i = 1; i < 7; i++) {
+          tempSum = 0;
+          for(let j = 0; j < dieArray.length; j++) {
+            if(dieArray[j] === i) continue
+            if(dieArray[j] === 7 - i) tempSum += 2
+            else tempSum += 1
+          }
+          possibleSolutions.push(tempSum)
+        }
+        
+        return Math.min.apply(Math, possibleSolutions)
+      }
 }
