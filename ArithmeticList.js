8eb12f4496291f1da470db810edc5231c0a065dd
JoJava25 <trogdor2k5@hotmail.com>
2023-11-13 23:13:00 -0500

    Refactor

--- ArithmeticList.js
+++ ArithmeticList.js
@@ -1,3 +1,9 @@
 let seqlist =function(first,c,l){
-    
+    let arr = [first]
+    let add = first
+    for(i = 1; i < l; i++){
+        add = add + c
+        arr.push(add)
+    }
+    return arr
 }
