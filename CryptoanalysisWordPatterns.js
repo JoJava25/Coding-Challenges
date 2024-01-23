39ea70d75ef34d71f8b56fc50e93f0ec4fe34cc8
JoJava25 <trogdor2k5@hotmail.com>
2024-01-23 16:52:00 -0500

    Refactor

--- CryptoanalysisWordPatterns.js
+++ CryptoanalysisWordPatterns.js
@@ -1,3 +1,15 @@
 function wordPattern(word) {
-    return; 
+    let read = {}
+    let count = 0
+    const pattern = word.split('').map(char => {
+      char = char.toLowerCase()
+      if (read.hasOwnProperty(char)) {
+        return read[char]
+      }
+      read[char] = count
+      let newCount = count
+      count++
+      return newCount
+    })
+    return pattern.join('.'); 
   }
