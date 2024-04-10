9dd705963592cdf8b3935ba8b81b2d09d5568bb2
JoJava25 <trogdor2k5@hotmail.com>
2024-04-09 21:56:16 -0400

    Refactor

--- HistogramH1.js
+++ HistogramH1.js
@@ -1,8 +1,8 @@
 function histogram(results) {
-    var histogram = '';
+    var histogram = ''
     
     for (var i=5; i>=0; --i)
-      histogram += (i+1) + '|' + '#'.repeat(results[i]) + (results[i] > 0 ? ' ' + results[i] : '') + "\n";
+      histogram += (i+1) + '|' + '#'.repeat(results[i]) + (results[i] > 0 ? ' ' + results[i] : '') + "\n"
     
-    return histogram;
+    return histogram
   }
