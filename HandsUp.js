4c2ae9a6226736a7d6b5e70abff43c9fe3e3d37e
JoJava25 <trogdor2k5@hotmail.com>
2024-04-17 23:38:17 -0400

    Refactor

--- HandsUp.js
+++ HandsUp.js
@@ -1,12 +1,15 @@
 const getPositions = s => {
     let d=s%27
     let a=0;
+
     if (d>8) a=1;
     if (d>17) a=2;
     d%=9;
     let b=0;
+
     if (d>2) b=1;
     if (d>5) b=2;
     d%=3
+    
     return [d,b,a]
   }
