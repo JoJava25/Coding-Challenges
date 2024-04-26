c1c4782fd7e3580512c9da419d6cc30af95fd92d
JoJava25 <trogdor2k5@hotmail.com>
2024-04-25 22:26:05 -0400

    Refactor

--- KillerGarage.js
+++ KillerGarage.js
@@ -2,19 +2,19 @@ const door = events => {
     let result = '',
       position = 0,
       moving = 0,
-      direction = 1;
+      direction = 1
   
     for (let event of events.split('')) {
-      if (event == 'O') direction *= -1;
-      else if (event == 'P') moving ^= 1;
+      if (event == 'O') direction *= -1
+      else if (event == 'P') moving ^= 1
   
-      let tmp = position + moving * direction;
+      let tmp = position + moving * direction
       if (tmp < 0 || tmp > 5) {
-        moving = 0;
-        direction *= -1;
-      } else position = tmp;
+        moving = 0
+        direction *= -1
+      } else position = tmp
   
-      result += position;
+      result += position
     }
-    return result;
+    return result
   }
