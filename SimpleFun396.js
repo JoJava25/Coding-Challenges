b04fdc9d268de75015f3a22bf5b3060c2b46f71d
JoJava25 <trogdor2k5@hotmail.com>
2025-01-09 17:57:01 -0500

    Refactor

--- SimpleFun396.js
+++ SimpleFun396.js
@@ -2,5 +2,6 @@ const longestSubstringOf = ([...s], visited = new Map(), start = 0) => s.reduce(
     if (visited[curr] != undefined) start = Math.max(visited[curr] + 1, start);   
     if (res.length < end - start + 1) res = s.slice(start, end + 1); 
     visited[curr] = end;
+    
     return res;
   }, '').length;
