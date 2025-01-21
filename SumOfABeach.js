3c0e1c26a744b9c7063643b6269753d0368ae694
JoJava25 <trogdor2k5@hotmail.com>
2025-01-20 23:19:30 -0500

    Refactor

--- SumOfABeach.js
+++ SumOfABeach.js
@@ -2,6 +2,7 @@ function sumOfABeach(beach) {
     let result = 0
     beach = beach.toLowerCase()
     let elements = ["sand", "water", "fish", "sun"]
+    
     for (let i = 0; i < 4; i++) {
         while (beach.includes(elements[i])) {
             beach = beach.replace(elements[i], '')
