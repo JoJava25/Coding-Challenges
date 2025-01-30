63a81e8b3fa8df1a7ba9211a9ceecb8579c339c6
JoJava25 <trogdor2k5@hotmail.com>
2025-01-30 15:53:22 -0500

    New solution

---
+++ CaffineScript.js
@@ -0,0 +1,14 @@
+function caffeineBuzz(n){
+    if(n % 3 == 0 && n % 4 == 0){
+        if(n % 2 == 0){
+            return "CoffeeScript"
+        }
+        return "Coffee"
+    }else if(n % 3 == 0){
+        if(n % 2 == 0){
+            return "JavaScript"
+        }
+        return "Java"
+    }
+    return "mocha_missing!"
+}
