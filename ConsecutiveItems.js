a710db7ba1178776aa5d728c8c43ec724e2528ee
JoJava25 <trogdor2k5@hotmail.com>
2025-02-17 23:55:57 -0500

    Refactor

--- ConsecutiveItems.js
+++ ConsecutiveItems.js
@@ -1,7 +1,6 @@
 function consecutive(arr, a, b) {
     const arrA = arr.indexOf(a)
     const arrB = arr.indexOf(b)
-    
     if(arrA + 1 == arrB || arrB + 1 == arrA){
       return true
     }else{
