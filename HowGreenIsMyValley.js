c09a4a5acbb07cd673959501ea58e334e847a927
JoJava25 <trogdor2k5@hotmail.com>
2025-02-25 00:39:15 -0500

    Complete

--- HowGreenIsMyValley.js
+++ HowGreenIsMyValley.js
@@ -1,4 +1,14 @@
 function makeValley(arr) {
-    let sorted = arr.sort((a, b) => a - b)
-    return sorted
-}
+    arr.sort((a, b)=> b - a )
+    const arr1 = [], arr2 = []
+    
+    for ( let i = 0; i < arr.length; i++ ){
+      if ( i % 2 ){
+        arr2.push(arr[i])
+      } else {
+        arr1.unshift(arr[i])
+      }
+    }
+    
+    return (arr2.concat(arr1)).reverse()
+  }
