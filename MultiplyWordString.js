4aeee2ba6e5cd7978025ca984b5662bac14d55be
JoJava25 <trogdor2k5@hotmail.com>
2025-03-12 18:50:00 -0400

    Refactor

--- MultiplyWordString.js
+++ MultiplyWordString.js
@@ -2,9 +2,10 @@ function modifyMultiply (str,loc,num) {
     let arr = str.split(' ')
     let res = []
     let strres =  ""
+
     for (i =0; i<num; i++){
-     res.push(arr[loc]);
-     strres = res.join('-');
+      res.push(arr[loc]);
+      strres = res.join('-');
     }
     return strres
   } 
